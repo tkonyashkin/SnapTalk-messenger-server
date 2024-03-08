ff1b24fa27a821979b1e1cf8676a92dc29bfc22f
tkonyashkin <95534188+tkonyashkin@users.noreply.github.com>
2024-03-08 16:35:30 +0300

    decomposition-fix

---
+++ src/core/messenger_service.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpc/grpc.h>
+#include <grpcpp/security/server_credentials.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
+
+#include "../api/messenger.grpc.pb.h"
+
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::ServerReader;
+using grpc::ServerReaderWriter;
+using grpc::ServerWriter;
+using grpc::Status;
+
+using namespace messenger;
+
+class MessengerServiceImpl final : public MessengerService::Service {
+public:
+    
+};
